2abce11978e6c86f4a9ce3da33f520ac0993ce8b
ufomzd <ufomzd@gmail.com>
2018-12-20 19:35:07 +0800

    最大化打开目录
    批处理真的不错。
    open 打开命令
    gre这东西，主要是因为我在网上下载了一个grep GNU的，跟我自己的grep冲突了，所以
    这里把我的grep给变成了gre了。  ex就是最大化打开的命令。
    
    Signed-off-by: ufomzd <ufomzd@gmail.com>

---
+++ ex.bat
@@ -0,0 +1,2 @@
+@echo off
+start /max "" explorer.exe "%1" 
---
+++ open.bat
@@ -0,0 +1,26 @@
+@echo off
+
+set a=%1
+if "%a%"== "?" (
+	echo qt		qt out
+	echo project	project
+	echo workbench	workbench
+	echo s3		tws s3 project folder
+	echo em031	em031 project folder
+	echo git		git folder
+	echo ...
+) else if "%a%" == "qt" (
+	call ex D:\qt\out
+) else if "%a%" == "project" (
+	call ex D:\project
+) else if "%a%" == "workbench" (
+	call ex D:\workbench
+) else if "%a%" == "s3" (
+	call ex D:\project\2_tws_s3
+) else if "%a%" == "em031" (
+	call ex D:\project\3_EM031 
+) else if "%a%" == "git" (
+	call ex D:\git
+) else (
+	echo *£þ¦á£þ
+)
